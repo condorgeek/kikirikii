6561dd85538d84970a04461c2146b849cd6f8ac6
mkr <marcelo.krebber@twt-dh.de>
2019-01-17 15:38:03 +0000

    support for public/** REST requests

--- src/main/java/com/kikirikii/security/configuration/CustomCorsFilter.java
+++ src/main/java/com/kikirikii/security/configuration/CustomCorsFilter.java
@@ -41,7 +41,8 @@ public class CustomCorsFilter extends CorsFilter {
         source.registerCorsConfiguration("/public/login", config);
         source.registerCorsConfiguration("/public/token", config);
         source.registerCorsConfiguration("/public/validate/**", config);
-        source.registerCorsConfiguration("/public/user/**", config);
+//        source.registerCorsConfiguration("/public/user/**", config);
+        source.registerCorsConfiguration("/public/**", config);
         source.registerCorsConfiguration("/stomp/**", config);
 
         return source;
